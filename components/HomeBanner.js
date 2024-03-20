412a3e0b4f1952cbff207289a0cd5611185a5bf8
juanfel254 <jparrar@unal.edu.co>
2024-03-20 17:28:52 -0500

    Home Banner updated

--- components/HomeBanner.js
+++ components/HomeBanner.js
@@ -2,27 +2,29 @@ import Image from "next/image";
 
 export default function HomeBanner() {
   return (
-    <section className="-my-60 md:-my-36 flex flex-col w-full h-screen items-center justify-center text-center gap-7">
-      <h1 className="font-extrabold text-5xl lg:text-8xl">
+    <section className="-my-44 md:-my-36 flex flex-col w-full h-screen items-center justify-center text-center gap-5 md:gap-7">
+      <h1 className="font-extrabold text-4xl md:text-5xl lg:text-8xl">
         Juan <span className="text-sky-400">Felipe Parra</span> Runceria
       </h1>
-      <div className="flex gap-3 items-center">
-        <h2 className="text-lg md:text-3xl">
-          Representante Estudiantil de la Facultad de Ingeniería
-        </h2>
-        <h2 className="font-black text-lg md:text-4xl">UNAL</h2>
-      </div>
-      <div className="flex gap-4 items-center">
-        <Image
-          width={100}
-          height={100}
-          src="/icons/tigui.svg"
-          alt="Tigüi S.A.S"
-          className="-mb-1"
-        />
-        <h2 className="text-lg md:text-3xl">
-          Software CTO & Desarrollador Web
-        </h2>
+      <div className="flex flex-col-reverse md:flex-col gap-2 md:gap-5">
+        <div>
+          <h2 className="text-sm md:text-3xl">
+            Representante Estudiantil de la Facultad de Ingeniería{" "}
+            <span className="font-black text-lg md:text-4xl">UNAL</span>
+          </h2>
+        </div>
+        <div className="flex w-full justify-center items-center gap-2">
+          <Image
+            width={100}
+            height={100}
+            src="/icons/tigui.svg"
+            alt="Tigüi S.A.S"
+            className="-mb-1 size-12 md:size-auto"
+          />
+          <h2 className="text-sm md:text-3xl">
+            Software CTO & Desarrollador Web
+          </h2>
+        </div>
       </div>
     </section>
   );

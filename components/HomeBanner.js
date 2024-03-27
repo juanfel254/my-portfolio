3df00974a1098c40728b9a96c57144773ed0aeff
juanfel254 <jparrar@unal.edu.co>
2024-03-26 19:37:34 -0500

    Title text

--- components/HomeBanner.js
+++ components/HomeBanner.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function HomeBanner() {
   return (
     <section className="-my-44 md:-my-36 flex flex-col w-full h-screen items-center justify-center text-center gap-5 md:gap-7">
-      <h1 className="font-extrabold text-4xl md:text-5xl lg:text-8xl">
+      <h1 className="font-extrabold text-4xl md:text-8xl">
         Juan <span className="text-sky-400">Felipe Parra</span> Runceria
       </h1>
       <div className="flex flex-col-reverse md:flex-col gap-2 md:gap-5">

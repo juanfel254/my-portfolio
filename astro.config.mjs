0755a4e0047ee64303bc41aad33b50a6e44df9d4
Juan Felipe Parra Runceria <jparrar@unal.edu.co>
2024-08-23 23:53:09 -0500

    chore: Update Astro configuration to use @astrojs/vercel for serverless output and enable web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juanfe.vercel.app',
   integrations: [tailwind(), react()],
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
